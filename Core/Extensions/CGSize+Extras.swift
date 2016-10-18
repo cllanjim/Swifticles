604c70f63f8f73c8eeb9c6ec234e68c53c0787e5
Nick Raptis <nraptis@gmail.com>
2016-10-18 00:37:14 -0700

    Added 21 new asset catalogs to Bounce project. Hopefully this will get it looking better as we move forward!

--- Core/Extensions/CGSize+Extras.swift
+++ Core/Extensions/CGSize+Extras.swift
@@ -8,9 +8,8 @@
 
 import UIKit
 
-extension CGSize {
-    
-    
+extension CGSize
+{
     func getAspectFit(_ size: CGSize) -> (size: CGSize, scale: CGFloat) {
         var result = (size: CGSize(width: width, height: height), scale: CGFloat(1.0))
         if width > Math.epsilon && height > Math.epsilon && size.width > Math.epsilon && size.height > Math.epsilon {
@@ -42,40 +41,6 @@ extension CGSize {
         }
         return result
     }
-    
-    /*
-     - (FrameInfo *)frameInfoAspectFit:(CGSize)videoSize forFrame:(CGRect)screenRect{
-     FrameInfo *info = [[FrameInfo alloc] init];
-     
-     info.scale = 1.0f;
-     info.size = CGSizeMake(screenRect.size.width, screenRect.size.height);
-     if(videoSize.width > 0 && videoSize.height > 0 && screenRect.size.width > 0 && screenRect.size.height > 0)
-     {
-     if((videoSize.width / videoSize.height) > (screenRect.size.width / screenRect.size.height))info.scale = screenRect.size.width / videoSize.width;
-     else info.scale = screenRect.size.height / videoSize.height;
-     info.size = CGSizeMake(videoSize.width * info.scale, videoSize.height * info.scale);
-     }
-     
-     return info;
-     }
-     
-     - (FrameInfo *)frameInfoAspectFill:(CGSize)videoSize forFrame:(CGRect)screenRect{
-     FrameInfo *info = [[FrameInfo alloc] init];
-     
-     info.scale = 1.0f;
-     info.size = CGSizeMake(screenRect.size.width, screenRect.size.height);
-     if(videoSize.width > 0 && videoSize.height > 0 && screenRect.size.width > 0 && screenRect.size.height > 0)
-     {
-     if((videoSize.width / videoSize.height) < (screenRect.size.width / screenRect.size.height))info.scale = screenRect.size.width / videoSize.width;
-     else info.scale = screenRect.size.height / videoSize.height;
-     info.size = CGSizeMake(videoSize.width * info.scale, videoSize.height * info.scale);
-     }
-     
-     return info;
-     }
-     */
-    
-    
 }
 
 
